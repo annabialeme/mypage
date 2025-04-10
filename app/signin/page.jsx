d57868fc0526265e173e89f9707c203c03a91a18
anna leme <162629023+annabialeme@users.noreply.github.com>
2025-04-10 16:24:57 -0300

    feat: create signin

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -5,17 +5,20 @@ import styles from "../styles/Signin.module.css";
 export default function Home() {
     return (
         <div className={styles.login}>
-            <h1 className={styles.home}>Início</h1>
-            <p className={styles.name}>Digite seu nome:</p>
-            <button className={styles.name}>Digite aqui</button>
+            <h1 className={styles.home}>Log In</h1>
+            <p className={styles.conta}>Don't have an account?</p>
+            <p className={styles.name}>Email Adress</p>
+            <input className={styles.input} type= "text" id = "taskInput" placeholder=""></input>
+            
 
             <div className={styles.email}>
-                <p>Digite seu e-mail</p>
-                <button className={styles.name}>Digite aqui</button>
-            </div>
-            <div className={styles.senha}>
-                <p>Digite sua senha</p>
-                <button className={styles.name}>Digite aqui</button>
+                <p>Password</p>
+                <input className={styles.input} type= "text" id = "taskInput" placeholder=""></input>
+
+                <button className={styles.button}>Log in</button>
+
+                <p className={styles.senha}>Forgot your password?</p>
+
             </div>
         </div>
     )
